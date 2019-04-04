6ea7736d497ef093e6ac755c93290626535db7c1
paintedlbird7 <rosaperezlarios@gmail.com>
2019-04-04 12:01:54 -0700

    completed mpv checking now

--- posts/posts-route.js
+++ posts/posts-route.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const db = require('./posts-model.js');
+const Posts = require('./posts-model.js');
 
 const router = express.Router();
 
@@ -20,102 +20,102 @@ router.get('/', async (req, res) => {
     }
   });
   
-//   router.get('/:id', async (req, res) => {
-//     try {
-//       const post = await Posts.findById(req.params.id);
+  router.get('/:id', async (req, res) => {
+    try {
+      const post = await Posts.findById(req.params.id);
   
-//       if (post) {
-//         res.status(200).json(post);
-//       } else {
-//         res.status(404).json({ message: 'Post not found' });
-//       }
-//     } catch (error) {
-//       // log error to database
-//       console.log(error);
-//       res.status(500).json({
-//         message: 'Error retrieving the post',
-//       });
-//     }
-//   });
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res.status(404).json({ message: 'Post not found' });
+      }
+    } catch (error) {
+      // log error to database
+      console.log(error);
+      res.status(500).json({
+        message: 'Error retrieving the post',
+      });
+    }
+  });
   
-//   router.get('/:id/posts', async (req, res) => {
-//     try {
-//       const posts = await Posts.findPostMessages(req.params.id);
+  router.get('/:id/posts', async (req, res) => {
+    try {
+      const posts = await Posts.findPostMessages(req.params.id);
   
-//       if (posts.length > 0) {
-//         res.status(200).json(posts);
-//       } else {
-//         res.status(404).json({ post: 'No messages for this post' });
-//       }
-//     } catch (error) {
-//       // log error to database
-//       console.log(error);
-//       res.status(500).json({
-//         message: 'Error retrieving the messages for this post',
-//       });
-//     }
-//   });
+      if (posts.length > 0) {
+        res.status(200).json(posts);
+      } else {
+        res.status(404).json({ post: 'No messages for this post' });
+      }
+    } catch (error) {
+      // log error to database
+      console.log(error);
+      res.status(500).json({
+        message: 'Error retrieving the messages for this post',
+      });
+    }
+  });
   
-//   router.post('/', async (req, res) => {
-//     try {
-//       const post = await Posts.add(req.body);
-//       res.status(201).json(post);
-//     } catch (error) {
-//       // log error to database
-//       console.log(error);
-//       res.status(500).json({
-//         message: 'Error adding the post',
-//       });
-//     }
-//   });
+  router.post('/', async (req, res) => {
+    try {
+      const post = await Posts.add(req.body);
+      res.status(201).json(post);
+    } catch (error) {
+      // log error to database
+      console.log(error);
+      res.status(500).json({
+        message: 'Error adding the post',
+      });
+    }
+  });
   
-//   router.delete('/:id', async (req, res) => {
-//     try {
-//       const count = await Posts.remove(req.params.id);
-//       if (count > 0) {
-//         res.status(200).json({ message: 'The post has been nuked' });
-//       } else {
-//         res.status(404).json({ message: 'The post could not be found' });
-//       }
-//     } catch (error) {
-//       // log error to database
-//       console.log(error);
-//       res.status(500).json({
-//         message: 'Error removing the post',
-//       });
-//     }
-//   });
+  router.delete('/:id', async (req, res) => {
+    try {
+      const count = await Posts.remove(req.params.id);
+      if (count > 0) {
+        res.status(200).json({ message: 'The post has been nuked' });
+      } else {
+        res.status(404).json({ message: 'The post could not be found' });
+      }
+    } catch (error) {
+      // log error to database
+      console.log(error);
+      res.status(500).json({
+        message: 'Error removing the post',
+      });
+    }
+  });
   
-//   router.put('/:id', async (req, res) => {
-//     const changes = req.body;
-//     try {
-//       const post = await Posts.update(req.params.id, req.body);
-//       if (post) {
-//         res.status(200).json(post);
-//       } else {
-//         res.status(404).json({ message: 'The post could not be found' });
-//       }
-//     } catch (error) {
-//       // log error to database
-//       console.log(error);
-//       res.status(500).json({
-//         message: 'Error updating the post',
-//       });
-//     }
-//   });
+  router.put('/:id', async (req, res) => {
+    const changes = req.body;
+    try {
+      const post = await Posts.update(req.params.id, req.body);
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res.status(404).json({ message: 'The post could not be found' });
+      }
+    } catch (error) {
+      // log error to database
+      console.log(error);
+      res.status(500).json({
+        message: 'Error updating the post',
+      });
+    }
+  });
   
-//   server.post('/:id/posts', async (req, res) => {
-//       const postInfo = { ...req.body, post_id: req.params.id }
-//       try {
-//           const post = await Posts.addPost(postInfo)
-//           res.status(201).json(message)
-//       } catch(error) {
-//           console.log(error)
-//           res.status(500).json({
-//               message: 'Errrrroorrrrrrrr'
-//           })
-//       }
-//   })
+  // server.post('/:id/posts', async (req, res) => {
+  //     const postInfo = { ...req.body, post_id: req.params.id }
+  //     try {
+  //         const post = await Posts.addPost(postInfo)
+  //         res.status(201).json(message)
+  //     } catch(error) {
+  //         console.log(error)
+  //         res.status(500).json({
+  //             message: 'Errrrroorrrrrrrr'
+  //         })
+  //     }
+  // })
         
 
 
